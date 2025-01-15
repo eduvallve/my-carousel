24ad15d5bc7982786da2dae0ebcbde41a202b757
Eduard Vallvé <66818781+eduvallve@users.noreply.github.com>
2025-01-15 08:03:25 +0100

    Basic plugin structure

---
+++ my-carousel.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Plugin Name: My Carousel
+* Plugin URI: https://eduvallve.com
+* Description: Custom WordPress plugin for carousels.
+* Version: 0.1
+* Author: eduvallve
+* Author URI: https://eduvallve.com
+**/
+
+/**
+ * Public (Front-office)
+ */
+
+require_once 'public/public.php';
+
+/**
+ * Admin page (Back-office)
+ */
+
+require_once 'admin/admin.php';
+
+?>
