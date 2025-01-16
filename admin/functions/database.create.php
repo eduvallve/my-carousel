8cd85b2f6575b8300dceb6b0399a02193bb17630
Eduard Vallvé <66818781+eduvallve@users.noreply.github.com>
2025-01-16 16:09:26 +0100

    MC - Improve database capacity

--- admin/functions/database.create.php
+++ admin/functions/database.create.php
@@ -3,10 +3,10 @@ function createMyCarouselTable() {
     $table = $GLOBALS['cfgMc']['table'];
     $query_createMyCarousel_table = "CREATE TABLE IF NOT EXISTS $table (
         id int NOT NULL AUTO_INCREMENT,
-        name varchar(255) NOT NULL,
-        content varchar(5120) NOT NULL,
-        styles varchar(255) NOT NULL,
-        params varchar(2048) NOT NULL,
+        name text NOT NULL,
+        content longtext NOT NULL,
+        styles longtext NOT NULL,
+        params longtext NOT NULL,
         active bool NOT NULL,
         PRIMARY KEY (id)
     ) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
