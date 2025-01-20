41c12835610c71bf879bb70ca33adc1512d0cc97
Eduard Vallvé <66818781+eduvallve@users.noreply.github.com>
2025-01-20 12:04:14 +0100

    MC - Remove mandatory fields asterisk

--- admin/components/card.php
+++ admin/components/card.php
@@ -11,7 +11,7 @@
     </div>
     <label for="mc__card__img">
         <span class="dashicons dashicons-format-image"></span>
-        <input type="url" id="mc__card__img" name="mc__card__img[]" placeholder="URL image from Media *" <?php echo isset($img) ? 'value="'.$img.'"' : '' ; ?>>
+        <input type="url" id="mc__card__img" name="mc__card__img[]" placeholder="URL image from Media" <?php echo isset($img) ? 'value="'.$img.'"' : '' ; ?>>
     </label>
     <label for="mc__card__img__alt">
         <input type="text" id="mc__card__img__alt" name="mc__card__img__alt[]" placeholder="Image 'Alt' text" <?php echo isset($alt) ? 'value="'.$alt.'"' : '' ; ?>>
@@ -19,7 +19,7 @@
     </label>
     <label for="mc__card__title">
         <span class="dashicons dashicons-editor-textcolor"></span>
-        <input type="text" id="mc__card__title" name="mc__card__title[]" placeholder="Title *" <?php echo isset($title) ? 'value="'.$title.'"' : '' ; ?>>
+        <input type="text" id="mc__card__title" name="mc__card__title[]" placeholder="Title" <?php echo isset($title) ? 'value="'.$title.'"' : '' ; ?>>
     </label>
     <label for="mc__card__subtitle">
         <span class="dashicons dashicons-text"></span>
@@ -27,10 +27,10 @@
     </label>
     <label for="mc__card__btn">
         <span class="dashicons dashicons-button"></span>
-        <input type="text" id="mc__card__btn" name="mc__card__btn[]" placeholder="Button text *" <?php echo isset($btn) ? 'value="'.$btn.'"' : '' ; ?>>
+        <input type="text" id="mc__card__btn" name="mc__card__btn[]" placeholder="Button text" <?php echo isset($btn) ? 'value="'.$btn.'"' : '' ; ?>>
     </label>
     <label for="mc__card__btn__url">
         <span class="dashicons dashicons-admin-links"></span>
-        <input type="url" id="mc__card__btn__url" name="mc__card__btn__url[]" placeholder="Card pointing URL *" <?php echo isset($url) ? 'value="'.$url.'"' : '' ; ?>>
+        <input type="url" id="mc__card__btn__url" name="mc__card__btn__url[]" placeholder="Card pointing URL" <?php echo isset($url) ? 'value="'.$url.'"' : '' ; ?>>
     </label>
 </li>
